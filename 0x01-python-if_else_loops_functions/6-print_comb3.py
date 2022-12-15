da7688f7bf3c4f4b1ddc339def9f039f7732bc82
ugcinikhaya <gcinikhayamthethwa@gmail.com>
2022-12-15 20:39:12 +0000

    If Else Loops

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for digit1st in range(0, 10):
-    for digit2nd in range(digit1st +1, 10):
+    for digit2nd in range(digit1st + 1, 10):
         if digit1st == 8 and digit2nd == 9:
             print("{}{}".format(digit1st, digit2nd))
         else:
